5047e2a0199be0b02012c4f1f1651e95182114f9
meows <b5c6@protonmail.com>
2022-07-16 07:54:15 -0700

    init table and loads and works decently
    Signed-off-by: meows <b5c6@protonmail.com>

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,37 @@
-import { GetBacon } from './utils';
+import {GetHeaders} from './utils';
 
-const baconEl = document.querySelector('.bacon');
+const tableBody = document.querySelector('#mytable-body');
+const loadingIcon = document.querySelector('#loading-icon');
+const empyUnclesHash = "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347"
 
-GetBacon()
-  .then(res => {
-    const markup = res.reduce((acc, val) => (acc += `<p>${val}</p>`), '');
-    baconEl.innerHTML = markup;
-  })
-  .catch(err => (baconEl.innerHTML = err));
+// makeHeaderTableRow makes a table row for a header.
+// It returns HTML.
+function makeHeaderTableRow(headerJSON, lastOfDupes) {
+    return `
+<tr class="${headerJSON.orphan ? 'orphan' : 'canon'} ${headerJSON.sha3Uncles !== empyUnclesHash ? 'uncler' : ''} ${lastOfDupes ? 'dupeEnd' : ''}">
+    <td>${headerJSON.number}</td>
+    <td>${headerJSON.timestamp}</td>
+    <td>${headerJSON.miner.substring(0, 10)}</td>
+    <td>${headerJSON.gasUsed}</td>
+    <td>${headerJSON.hash.substring(0, 10)}</td>
+    <td>${headerJSON.uncleBy.substring(0, 10)}</td>
+</tr>`;
+}
+
+GetHeaders()
+    .then(res => {
+        loadingIcon.style.display = 'none';
+        tableBody.innerHTML = "";
+
+        for (let i = 0; i < res.length; i++) {
+            const header = res[i];
+
+            // If this is the last of a set of duplicates, add a class to the row.
+            const lastOfDupes = i === res.length - 1 || res[i + 1].number !== header.number;
+
+            const row = makeHeaderTableRow(header, lastOfDupes);
+            tableBody.innerHTML += row;
+        }
+
+    })
+    .catch(err => (tableBody.innerHTML = err));
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,7 +1,37 @@
-export const GetBacon = () => {
-  const body = fetch('https://baconipsum.com/api/?type=all-meat&paras=3').then(
+export const GetHeaders = () => {
+  const body = fetch('https://classic.orphans.etccore.in/api/headers?limit=100').then(
     res => res.json()
-  );
+  ).catch((err) => {
+    console.log(err);
+    return Promise.reject(err);
+  });
 
   return body;
 };
+
+/*
+ "uptime": 324,
+  "chain_id": 61,
+  "latest_header": {
+        "created_at": "0001-01-01T00:00:00Z",
+        "updated_at": "0001-01-01T00:00:00Z",
+        "hash": "0x4018a7851f87ac7c7c7da1549aa11717979acaaef8937e67b1db3a573e5df29a",
+        "parentHash": "0x742fe6c7bb519a9209fb1ab4a69e9133b34b7926bebd62b100033f6f60ed89e4",
+        "sha3Uncles": "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347",
+        "miner": "0xDf7D7e053933b5cC24372f878c90E62dADAD5d42",
+        "stateRoot": "0xf9df79e74c9f87a3774bdc52ece20837314e9579f831006a85c23adbe16a32d9",
+        "transactionsRoot": "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
+        "receiptsRoot": "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
+        "difficulty": "267018370939767",
+        "number": 15536588,
+        "gasLimit": 8031275,
+        "gasUsed": 0,
+        "timestamp": 1657896534,
+        "extraData": "c3RyYXR1bS1hc2lhLTE=",
+        "mixHash": "0x5e7b903556dcaa4a738152830194044b9a94f1ccf189a98146e5f66af81c96ca",
+        "nonce": "14687018096225711779",
+        "baseFeePerGas": "<nil>",
+        "orphan": false,
+        "uncleBy": ""
+    }
+ */
