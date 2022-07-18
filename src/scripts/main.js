6a87a5e3b0c2a7003bebfa4c04f9dc81f7405c06
meows <b5c6@protonmail.com>
2022-07-18 14:21:52 -0700

    install tooltips for block info
    Signed-off-by: meows <b5c6@protonmail.com>

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 import {GetHeaders, GetStatus, ListenCheckbox} from './utils';
 import {initSortable} from "./sortable";
+import tippy from "tippy.js";
 
 const tableBody = document.querySelector('#mytable-body');
 const loadingIcon = document.querySelector('#loading-icon');
@@ -11,13 +12,19 @@ const statusUptime = document.querySelector('#status-uptime');
 
 const style = document.querySelector('#mystyle');
 
+
+function headerTableRowID(headerJSON) {
+    return `header-row-${headerJSON.hash}`;
+}
+
 // makeHeaderTableRow makes a table row for a header.
 // It returns HTML.
 function makeHeaderTableRow(headerJSON, lastOfDupes) {
     // headerJSON.orphan = true;
     // headerJSON.sha3Uncles = "set";
     const str = `
-<tr class="
+<tr id="${headerTableRowID(headerJSON)}"
+    class="
     ${headerJSON.orphan ? 'orphan' : 'canon'} 
     ${headerJSON.sha3Uncles !== empyUnclesHash ? 'uncler' : ''} 
     ${lastOfDupes ? 'dupeEnd' : ''}
@@ -56,6 +63,45 @@ function makeHeaderTableRowForCanonicalBlock(number) {
 
 let latestBlockNumber = 0;
 
+function makeJSONTableHTML(json) {
+    let str = ``;
+    str += `<table>`
+    str += `<tbody>`;
+
+    const m = new Map(Object.entries(json));
+    for (const [key, value] of m) {
+        if (key === "txes") {
+            continue;
+        }
+        str += `<tr>`;
+        str += `<td>${key}</td>`;
+        str += `<td>${value}</td>`;
+        str += `</tr>`;
+    }
+
+    if (m.has("txes")) {
+        str += `<tr>`;
+        str += `<td>txes</td>`;
+        str += `<td>${JSON.stringify(m.get("txes"))}</td>`;
+        str += `</tr>`;
+    }
+
+    str += `</tbody>`;
+    str += `</table>`;
+    return str;
+}
+
+function initMyTippy(header) {
+    tippy(`#${headerTableRowID(header)}`, {
+        content: makeJSONTableHTML(header), // JSON.stringify(header, null, 2),
+        allowHTML: true,
+        trigger: 'click',
+        placement: 'right-start',
+        interactive: true,
+        maxWidth: '500px',
+    });
+}
+
 GetStatus()
     .then(res => {
         res.latest_header["is_latest"] = true;
@@ -75,6 +121,7 @@ GetStatus()
             ObjParent.innerHTML = ObjParent.innerHTML.replace('<div><!--THIS DATA SHOULD BE REPLACED--></div>', statusRowHTML);
         }
 
+        initMyTippy(res.latest_header);
         statusChainID.innerHTML = `${res.chain_id}`;
         statusUptime.innerHTML = `${res.uptime}s`;
 
@@ -86,19 +133,23 @@ GetStatus()
                 // tableBody.innerHTML = "";
 
                 for (let i = 0; i < res.length; i++) {
-                    const header = res[i];
+                    let header = res[i];
+
+                    header.txes_count = header.txes?.length || 0;
 
                     // If this is the last of a set of duplicates, add a class to the row.
                     const lastOfDupes = i === res.length - 1 || res[i + 1].number !== header.number;
 
                     if (i === 0) {
-                        for (let j = latestBlockNumber -1; j > header.number; j--) {
+                        for (let j = latestBlockNumber - 1; j > header.number; j--) {
                             tableBody.insertAdjacentHTML('beforeend', makeHeaderTableRowForCanonicalBlock(j));
                         }
                     }
 
                     const row = makeHeaderTableRow(header, lastOfDupes);
                     tableBody.insertAdjacentHTML("beforeend", row);
+
+                    initMyTippy(header)
 
                     // Insert boring canonical rows between the interesting, contested blocks.
                     // These will be hidden by CSS.
@@ -119,7 +170,6 @@ GetStatus()
     .catch(err => {
         // tableRowStatus.innerHTML = err;
     });
-
 
 
 ListenCheckbox('fill-canonical', function (el) {
